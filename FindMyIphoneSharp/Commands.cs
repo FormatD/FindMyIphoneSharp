11e78c0dc53f468d3496d5443d603b11c03f056b
FormatDeng <goscan@live.cn>
2013-11-06 13:57:28 +0800

    fix some error in Command template

--- FindMyIphoneSharp/Commands.cs
+++ FindMyIphoneSharp/Commands.cs
@@ -4,10 +4,10 @@ namespace FindMyIphoneSharp
 {
     public class Commands
     {
-        private readonly static String INIT_CLIENT = "{\"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"inactiveTime\": 2147483647, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"personID\": 0, \"osVersion\": \"4.2.1\", \"appVersion\": \"1.4\"}}";
-        private readonly static String SEND_MESSAGE_TMPL = "{\"sound\": %s, \"text\": \"%s\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"device\": \"%s\", \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"%s\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}, \"subject\": \"%s\"}";
-        private readonly static String LOCK_DEVICE_TMPL = "{\"device\": \"%s\", \"passcode\": \"%s\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"oldPasscode\": \"\", \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"%s\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}}";
-        private readonly static String WIPE_DEVICE_TMPL = "{\"device\": \"%s\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"%s\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}}";
+        private const String _initClient = "{\"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"inactiveTime\": 2147483647, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"personID\": 0, \"osVersion\": \"4.2.1\", \"appVersion\": \"1.4\"}}";
+        private const String _sendMessageTmpl = "{\"sound\": {0}, \"text\": \"{1}\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"device\": \"{2}\", \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"{3}\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}, \"subject\": \"{4}\"}";
+        private const String _lockDeviceTmpl = "{\"device\": \"{0}\", \"passcode\": \"{1}\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"oldPasscode\": \"\", \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"{2}\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}}";
+        private const String _wipeDeviceTmpl = "{\"device\": \"{0}\", \"serverContext\": {\"maxLocatingTime\": 90000, \"deviceLoadStatus\": \"203\", \"prefsUpdateTime\": 1276872996660, \"lastSessionExtensionTime\": null, \"clientId\": \"0000000000000000000000000000000000000000\", \"timezone\": {\"previousOffset\": -28800000, \"currentOffset\": -25200000, \"previousTransition\": 1268560799999, \"tzCurrentName\": \"Pacific Daylight Time\", \"tzName\": \"America/Los_Angeles\"}, \"validRegion\": true, \"sessionLifespan\": 900000, \"preferredLanguage\": \"en\", \"hasDevices\": true, \"maxDeviceLoadTime\": 60000, \"callbackIntervalInMS\": 3000}, \"clientContext\": {\"deviceUDID\": \"0000000000000000000000000000000000000000\", \"selectedDevice\": \"{1}\", \"inactiveTime\": 5911, \"productType\": \"iPad1,1\", \"appName\": \"FindMyiPhone\", \"buildVersion\": \"145\", \"osVersion\": \"3.2\", \"appVersion\": \"1.4\", \"shouldLocate\": false}}";
 
         private Commands()
         {
@@ -16,7 +16,7 @@ namespace FindMyIphoneSharp
 
         public static String GetClientInitContext()
         {
-            return INIT_CLIENT;
+            return _initClient;
         }
 
         public static String GetSendMsgCmd(bool playSound, String message, String title, Device device)
@@ -31,7 +31,7 @@ namespace FindMyIphoneSharp
             {
                 throw new ArgumentException("Device Id should never be null.");
             }
-            return string.Format(SEND_MESSAGE_TMPL, playSound, msg, device.Id, device.Id, subject);
+            return string.Format(_sendMessageTmpl, playSound, msg, device.Id, device.Id, subject);
         }
 
         public static String GetLockDeviceCmd(String passcode, Device device)
@@ -45,7 +45,7 @@ namespace FindMyIphoneSharp
             {
                 throw new ArgumentException("Device Id should never be null.");
             }
-            return string.Format(LOCK_DEVICE_TMPL, device.Id, newPasscode, device.Id);
+            return string.Format(_lockDeviceTmpl, device.Id, newPasscode, device.Id);
         }
 
         public static String GetWipeDeviceCmd(Device device)
@@ -58,7 +58,7 @@ namespace FindMyIphoneSharp
             {
                 throw new ArgumentException("Device Id should never be null.");
             }
-            return string.Format(WIPE_DEVICE_TMPL, device.Id, device.Id);
+            return string.Format(_wipeDeviceTmpl, device.Id, device.Id);
         }
     }
 }
--- FindMyIphoneSharp/DeviceLocator.cs
+++ FindMyIphoneSharp/DeviceLocator.cs
@@ -15,7 +15,7 @@ namespace FindMyIphoneSharp
 {
     public class DeviceLocator
     {
-        private static readonly int DEFAULT_LOCATE_TIMEOUT = 300; // in seconds
+        private const int _defaultLocateTimeout = 300; // in seconds
         private readonly String _userName;
         private readonly String _password;
         private readonly List<Device> _devices = new List<Device>();
@@ -43,7 +43,11 @@ namespace FindMyIphoneSharp
             try
             {
                 var response = Post(string.Format(_initTmpl, _userName), Commands.GetClientInitContext());
-                var text = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                var responseStream = response.GetResponseStream();
+                if (responseStream == null)
+                    return;
+
+                var text = new StreamReader(responseStream).ReadToEnd();
                 var json = (JObject)JsonConvert.DeserializeObject(text);
                 //if (json.containsKey("error"))
                 //{
@@ -78,17 +82,19 @@ namespace FindMyIphoneSharp
             JObject loc = (JObject)deviceProps["location"];
             if (loc != null)
             {
-                LocationInfo deviceLocationInfo = new LocationInfo();
-                deviceLocationInfo.TimeStamp = ((long)loc["timeStamp"]);
-                deviceLocationInfo.LocationType = (loc["locationType"]);
-                deviceLocationInfo.IsLocationFinished = ((Boolean)loc["locationFinished"]);
-                deviceLocationInfo.HorizontalAccuracy = ((Double)loc["horizontalAccuracy"]);
-                deviceLocationInfo.PositionType = ((String)loc["positionType"]);
-                deviceLocationInfo.Longitude = ((Double)loc["longitude"]);
-                deviceLocationInfo.IsInaccurate = ((Boolean)loc["isInaccurate"]);
-                deviceLocationInfo.Latitude = ((Double)loc["latitude"]);
-                deviceLocationInfo.IsOld = ((Boolean)loc["isOld"]);
-                device.LocationInfo = (deviceLocationInfo);
+                LocationInfo deviceLocationInfo = new LocationInfo
+                {
+                    TimeStamp = (long)loc["timeStamp"],
+                    LocationType = loc["locationType"],
+                    IsLocationFinished = (Boolean)loc["locationFinished"],
+                    HorizontalAccuracy = (Double)loc["horizontalAccuracy"],
+                    PositionType = (String)loc["positionType"],
+                    Longitude = (Double)loc["longitude"],
+                    IsInaccurate = (Boolean)loc["isInaccurate"],
+                    Latitude = (Double)loc["latitude"],
+                    IsOld = (Boolean)loc["isOld"]
+                };
+                device.LocationInfo = deviceLocationInfo;
             }
             device.DeviceModel = ((String)deviceProps["deviceModel"]);
             JObject remLck = (JObject)deviceProps["remoteLock"];
@@ -116,28 +122,30 @@ namespace FindMyIphoneSharp
             JObject f = (JObject)deviceProps["features"];
             if (f != null)
             {
-                FeaturesInfo featuresInfo = new FeaturesInfo();
-                featuresInfo.IsCltEnabled = ((Boolean)f["CLT"]);
-                featuresInfo.IsCwpEnabled = ((Boolean)f["CWP"]);
-                featuresInfo.IsWmgEnabled = ((Boolean)f["WMG"]);
-                featuresInfo.IsXrmEnabled = ((Boolean)f["XRM"]);
-                featuresInfo.IsClkEnabled = ((Boolean)f["CLK"]);
-                featuresInfo.IsSndEnabled = ((Boolean)f["SND"]);
-                featuresInfo.IsLstEnabled = ((Boolean)f["LST"]);
-                featuresInfo.IsKeyEnabled = ((Boolean)f["KEY"]);
-                featuresInfo.IsWipEnabled = ((Boolean)f["WIP"]);
-                featuresInfo.IsLocEnabled = ((Boolean)f["LOC"]);
-                featuresInfo.IsLlcEnabled = ((Boolean)f["LLC"]);
-                featuresInfo.IsMsgEnabled = ((Boolean)f["MSG"]);
-                featuresInfo.IsLmgEnabled = ((Boolean)f["LMG"]);
-                featuresInfo.IsRemEnabled = ((Boolean)f["REM"]);
-                featuresInfo.IsLckEnabled = ((Boolean)f["LCK"]);
-                featuresInfo.IsSvpEnabled = ((Boolean)f["SVP"]);
-                featuresInfo.IsLklEnabled = ((Boolean)f["LKL"]);
-                featuresInfo.IsTeuEnabled = ((Boolean)f["TEU"]);
-                featuresInfo.IsPinEnabled = ((Boolean)f["PIN"]);
-                featuresInfo.IsLkmEnabled = ((Boolean)f["LKM"]);
-                featuresInfo.IsKpdEnabled = ((Boolean)f["KPD"]);
+                FeaturesInfo featuresInfo = new FeaturesInfo
+                {
+                    IsCltEnabled = ((Boolean)f["CLT"]),
+                    IsCwpEnabled = ((Boolean)f["CWP"]),
+                    IsWmgEnabled = ((Boolean)f["WMG"]),
+                    IsXrmEnabled = ((Boolean)f["XRM"]),
+                    IsClkEnabled = ((Boolean)f["CLK"]),
+                    IsSndEnabled = ((Boolean)f["SND"]),
+                    IsLstEnabled = ((Boolean)f["LST"]),
+                    IsKeyEnabled = ((Boolean)f["KEY"]),
+                    IsWipEnabled = ((Boolean)f["WIP"]),
+                    IsLocEnabled = ((Boolean)f["LOC"]),
+                    IsLlcEnabled = ((Boolean)f["LLC"]),
+                    IsMsgEnabled = ((Boolean)f["MSG"]),
+                    IsLmgEnabled = ((Boolean)f["LMG"]),
+                    IsRemEnabled = ((Boolean)f["REM"]),
+                    IsLckEnabled = ((Boolean)f["LCK"]),
+                    IsSvpEnabled = ((Boolean)f["SVP"]),
+                    IsLklEnabled = ((Boolean)f["LKL"]),
+                    IsTeuEnabled = ((Boolean)f["TEU"]),
+                    IsPinEnabled = ((Boolean)f["PIN"]),
+                    IsLkmEnabled = ((Boolean)f["LKM"]),
+                    IsKpdEnabled = ((Boolean)f["KPD"])
+                };
                 device.FeaturesInfo = (featuresInfo);
             }
             device.DeviceClass = ((String)deviceProps["deviceClass"]);
@@ -153,11 +161,13 @@ namespace FindMyIphoneSharp
             JObject msgObj = (JObject)deviceProps["msg"];
             if (msgObj != null)
             {
-                MsgInfo msgInfo = new MsgInfo();
-                msgInfo.StatusCode = ((String)msgObj["statusCode"]);
-                msgInfo.CreateTimestamp = ((long)msgObj["createTimestamp"]);
-                msgInfo.UserText = ((Boolean)msgObj["userText"]);
-                msgInfo.PlaySound = ((Boolean)msgObj["playSound"]);
+                MsgInfo msgInfo = new MsgInfo
+                {
+                    StatusCode = ((String)msgObj["statusCode"]),
+                    CreateTimestamp = ((long)msgObj["createTimestamp"]),
+                    UserText = ((Boolean)msgObj["userText"]),
+                    PlaySound = ((Boolean)msgObj["playSound"])
+                };
                 device.MsgInfo = msgInfo;
             }
             device.LostModeEnabled = ((Boolean)deviceProps["lostModeEnabled"]);
@@ -165,15 +175,17 @@ namespace FindMyIphoneSharp
             JObject lstDev = (JObject)deviceProps["lostDevice"];
             if (lstDev != null)
             {
-                LostDeviceInfo lostDeviceInfo = new LostDeviceInfo();
-                lostDeviceInfo.StatusCode = ((String)lstDev["statusCode"]);
-                lostDeviceInfo.CreateTimestamp = ((long)lstDev["createTimestamp"]);
-                lostDeviceInfo.Text = ((String)lstDev["text"]);
-                lostDeviceInfo.UserText = ((Boolean)lstDev["userText"]);
-                lostDeviceInfo.StopLostMode = ((Boolean)lstDev["stopLostMode"]);
-                lostDeviceInfo.OwnerNbr = ((String)lstDev["ownerNbr"]);
-                lostDeviceInfo.Sound = ((Boolean)lstDev["sound"]);
-                lostDeviceInfo.EmailUpdates = ((Boolean)lstDev["emailUpdates"]);
+                LostDeviceInfo lostDeviceInfo = new LostDeviceInfo
+                {
+                    StatusCode = ((String)lstDev["statusCode"]),
+                    CreateTimestamp = ((long)lstDev["createTimestamp"]),
+                    Text = ((String)lstDev["text"]),
+                    UserText = ((Boolean)lstDev["userText"]),
+                    StopLostMode = ((Boolean)lstDev["stopLostMode"]),
+                    OwnerNbr = ((String)lstDev["ownerNbr"]),
+                    Sound = ((Boolean)lstDev["sound"]),
+                    EmailUpdates = ((Boolean)lstDev["emailUpdates"])
+                };
                 device.LostDeviceInfo = (lostDeviceInfo);
             }
             device.UserInfo = _userInfo;
@@ -186,14 +198,14 @@ namespace FindMyIphoneSharp
 
         public Device Locate(Device device)
         {
-            return Locate(device, DEFAULT_LOCATE_TIMEOUT);
+            return Locate(device, _defaultLocateTimeout);
         }
 
         public Device Locate(Device device, int timeout)
         {
             if (device == null)
             {
-                throw new ArgumentNullException("Device should not be null.");
+                throw new ArgumentNullException("device");
             }
             if (timeout < 0)
             {
@@ -230,57 +242,46 @@ namespace FindMyIphoneSharp
             String fullUrl = string.Format("https://{0}{1}", (_partition ?? "fmipmobile.icloud.com"), url);
             HttpWebRequest request = WebRequest.CreateHttp(fullUrl);
 
-            try
-            {
-                request.Headers.Add("Content-type", "application/json; charset=utf-8");
-                request.Headers.Add("X-apple-find-api-ver", "2.0");
-                request.Headers.Add("X-apple-authscheme", "UserIdGuest");
-                request.Headers.Add("X-apple-realm-support", "1.0");
-                request.Headers.Add("User-agent", "Find iPhone/1.2 MeKit (iPad: iPhone OS/4.2.1)");
-                request.Headers.Add("X-client-name", "iPad");
-                request.Headers.Add("X-client-uuid", "0cf3dc501ff812adb0b202baed4f37274b210853");
-                request.Headers.Add("Accept-language", "en-us");
-                request.Headers.Add("Connection", "keep-alive");
-                String credentials = string.Format("{0}:{1}", _userName, _password);
-                String encodedAuth = GetBase64String(credentials);
-                request.Headers.Add("Authorization", "Basic " + encodedAuth);
-                request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.BypassCache);
+            request.Headers.Add("Content-type", "application/json; charset=utf-8");
+            request.Headers.Add("X-apple-find-api-ver", "2.0");
+            request.Headers.Add("X-apple-authscheme", "UserIdGuest");
+            request.Headers.Add("X-apple-realm-support", "1.0");
+            request.Headers.Add("User-agent", "Find iPhone/1.2 MeKit (iPad: iPhone OS/4.2.1)");
+            request.Headers.Add("X-client-name", "iPad");
+            request.Headers.Add("X-client-uuid", "0cf3dc501ff812adb0b202baed4f37274b210853");
+            request.Headers.Add("Accept-language", "en-us");
+            request.Headers.Add("Connection", "keep-alive");
+            String credentials = string.Format("{0}:{1}", _userName, _password);
+            String encodedAuth = GetBase64String(credentials);
+            request.Headers.Add("Authorization", "Basic " + encodedAuth);
+            request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.BypassCache);
 
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Method = "POST";
 
-                byte[] postData = Encoding.UTF8.GetBytes(body);//postDataStr即为发送的数据，格式还是和上次说的一样 
-                request.ContentLength = postData.Length;
-                var requestStream = request.GetRequestStream();
-                requestStream.Write(postData, 0, postData.Length);
+            byte[] postData = Encoding.UTF8.GetBytes(body);//postDataStr即为发送的数据，格式还是和上次说的一样 
+            request.ContentLength = postData.Length;
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(postData, 0, postData.Length);
 
-                var response = (HttpWebResponse)request.GetResponse();
+            var response = (HttpWebResponse)request.GetResponse();
 
-                //int responseCode = response.StatusCode;
-                //String responseText = connection.getResponseMessage();
-                ////ignores 330 error.
-                ////330 error raises when redirected response did not contains
-                ////any content. (expected: 204 No Contents)
-                //if (responseCode == 330)
-                //{
-                //    responseCode = 204;
-                //}
+            //int responseCode = response.StatusCode;
+            //String responseText = connection.getResponseMessage();
+            ////ignores 330 error.
+            ////330 error raises when redirected response did not contains
+            ////any content. (expected: 204 No Contents)
+            //if (responseCode == 330)
+            //{
+            //    responseCode = 204;
+            //}
 
-                //if (responseCode < 200 || responseCode >= 300)
-                //{
-                //    throw new InvalidResponseException(responseText);
-                //}
+            //if (responseCode < 200 || responseCode >= 300)
+            //{
+            //    throw new InvalidResponseException(responseText);
+            //}
 
-                return response;
-            }
-            catch (IOException e)
-            {
-                //if (connection != null)
-                //{
-                //    connection.disconnect();
-                //}
-                throw;
-            }
+            return response;
         }
 
         public void SendMessage(Device device, String msg, String title, bool playSound)
@@ -311,13 +312,23 @@ namespace FindMyIphoneSharp
     {
         public DeviceNotFoundException(string deviceId)
         {
+            DeviceId = deviceId;
+        }
+
+        public string DeviceId
+        {
+            get;
+            set;
         }
     }
 
     public class LocateTimeoutException : Exception
     {
+        public int Timeout { get; set; }
+
         public LocateTimeoutException(int timeout)
         {
+            Timeout = timeout;
         }
     }
 }
